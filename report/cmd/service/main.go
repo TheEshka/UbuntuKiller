27b2eb078b388dd2e35734208265c6234c7a9e77
Andrey Bulgaru <agbulgaru@avito.ru>
2021-10-12 15:30:49 +0300

    add sasl and tls for kafka

--- report/cmd/service/main.go
+++ report/cmd/service/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"github.com/Shopify/sarama"
 	"github.com/jmoiron/sqlx"
 	"log"
 	"net/http"
+	"os"
+	"report/internal/common"
 	"report/internal/worker"
 	"report/internal/worker/books_genre"
 	"report/internal/worker/books_return"
+	"strings"
 	"time"
 
 	"github.com/antelman107/net-wait-go/wait"
@@ -29,14 +34,16 @@ type Config struct {
 	DatabaseUser     string `default:"postgres" split_words:"true"`
 	DatabasePassword string `required:"true" split_words:"true"`
 	DatabaseName     string `default:"report" split_words:"true"`
-	KafkaHost        string `default:"kafka" split_words:"true"`
-	KafkaPort        int    `default:"9092" split_words:"true"`
-	KafkaGenresTopic string `default:"books-genres" split_words:"true"`
-	KafkaReturnsTopic string `default:"books-returns" split_words:"true"`
-	KafkaGenresDlqTopic string `default:"books-genres-dlq" split_words:"true"`
-	KafkaReturnsDlqTopic string `default:"books-returns-dlq" split_words:"true"`
-	KafkaGenresGroup string `default:"report-genres" split_words:"true"`
-	KafkaReturnsGroup string `default:"report-returns" split_words:"true"`
+	KafkaConnString        string `default:"kafka:9092" split_words:"true"`
+	KafkaSaslLogin   string `default:"njeb2phw" split_words:"true"`
+	KafkaSaslPassword string `required:"true" split_words:"true"`
+	KafkaTlsCaPath    string `default:"/app/kafkaCA.pem" split_words:"true"`
+	KafkaGenresTopic string `default:"njeb2phw-books-genres" split_words:"true"`
+	KafkaReturnsTopic string `default:"njeb2phw-books-returns" split_words:"true"`
+	KafkaGenresDlqTopic string `default:"njeb2phw-books-genres-dlq" split_words:"true"`
+	KafkaReturnsDlqTopic string `default:"njeb2phw-books-returns-dlq" split_words:"true"`
+	KafkaGenresGroup string `default:"njeb2phw-report-genres" split_words:"true"`
+	KafkaReturnsGroup string `default:"njeb2phw-report-returns" split_words:"true"`
 }
 
 func initDatabase(cfg Config) *sqlx.DB {
@@ -76,19 +83,47 @@ func initDatabase(cfg Config) *sqlx.DB {
 }
 
 func initKafka(cfg Config) sarama.Client {
-	if !wait.New(
-		wait.WithProto("tcp"),
-		wait.WithWait(200*time.Millisecond),
-		wait.WithBreak(50*time.Millisecond),
-		wait.WithDeadline(15*time.Second),
-		wait.WithDebug(true),
-	).Do([]string{fmt.Sprintf("%s:%d", cfg.KafkaHost, cfg.KafkaPort)}) {
-		log.Fatal("timeout waiting for kafka")
+	addresses := strings.Split(cfg.KafkaConnString, ",")
+	for _, address := range addresses {
+		hostPort := strings.Split(address, ":")
+		if !wait.New(
+			wait.WithProto("tcp"),
+			wait.WithWait(200*time.Millisecond),
+			wait.WithBreak(50*time.Millisecond),
+			wait.WithDeadline(15*time.Second),
+			wait.WithDebug(true),
+		).Do([]string{fmt.Sprintf("%s:%s", hostPort[0], hostPort[1])}) {
+			log.Fatal("timeout waiting for kafka")
+		}
+	}
+
+	caCert, err := os.ReadFile(cfg.KafkaTlsCaPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		RootCAs:            caCertPool,
+		InsecureSkipVerify: false,
+		ClientAuth: tls.NoClientCert,
 	}
 
 	kafkaConfig := sarama.NewConfig()
 	kafkaConfig.Producer.Return.Successes = true
-	kafkaClient, err := sarama.NewClient([]string{fmt.Sprintf("%s:%d", cfg.KafkaHost, cfg.KafkaPort)}, kafkaConfig)
+	kafkaConfig.Net.SASL.Enable = true
+	kafkaConfig.Net.SASL.User = cfg.KafkaSaslLogin
+	kafkaConfig.Net.SASL.Password = cfg.KafkaSaslPassword
+	kafkaConfig.Net.SASL.SCRAMClientGeneratorFunc = func() sarama.SCRAMClient { return &common.XDGSCRAMClient{HashGeneratorFcn: common.SHA512} }
+	kafkaConfig.Net.SASL.Mechanism = sarama.SASLTypeSCRAMSHA512
+	kafkaConfig.Net.SASL.Handshake = true
+	kafkaConfig.Version = sarama.V2_5_0_0
+	kafkaConfig.Metadata.Full = false
+	kafkaConfig.Net.TLS.Enable = true
+	kafkaConfig.Net.TLS.Config = tlsConfig
+	kafkaClient, err := sarama.NewClient(addresses, kafkaConfig)
 	if err != nil {
 		log.Fatalf("failed to connect to kafka: %v\n", err)
 	}
--- report/go.mod
+++ report/go.mod
@@ -13,4 +13,5 @@ require (
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
+	github.com/xdg-go/scram v1.0.2
 )
